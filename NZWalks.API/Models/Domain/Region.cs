48a47ec9c62bbdb17ec374478a7176858cb603d6
Muhammad Faizan <mfaizan09999@gmail.com>
2025-01-27 15:14:29 +0500

    Add project files.

---
+++ NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,11 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string? RegionImageURL { get; set; }
+
+    }
+}
